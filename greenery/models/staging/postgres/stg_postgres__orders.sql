b9a7a326fc798e49ba068e56ae2920076902311b
Markéta Trunkátová <m.trunkatova@gmail.com>
2023-01-21 14:38:23 +0100

    message

--- greenery/models/staging/postgres/stg_postgres__orders.sql
+++ greenery/models/staging/postgres/stg_postgres__orders.sql
@@ -1,4 +1,20 @@
-with src_orders as (
+
+SELECT order_id,
+       user_id,
+       promo_id,
+       address_id,
+       TRY_TO_TIMESTAMP(created_at) AS created_at,
+       TRY_TO_DOUBLE(order_cost) AS order_cost,
+       TRY_TO_DOUBLE(shipping_cost) AS shipping_cost,
+       tracking_id,
+       shipping_service,
+       TRY_TO_TIMESTAMP(estimated_delivery_at) AS estimated_delivery_at,
+       TRY_TO_TIMESTAMP(delivered_at) AS delivered_at,
+       status
+
+FROM {{ source('postgres', 'orders') }}
+
+/*with src_orders as (
     select * from {{ source('postgres', 'orders ') }}
     ),
 
@@ -21,4 +37,5 @@ from src_orders
     )
 
 select * from renamed_recast
+*/
 
